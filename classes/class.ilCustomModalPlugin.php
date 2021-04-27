41c6a434b8e6a1ef814010acb191cb32380e9aac
Marvin Barz <barz@leifos.de>
2021-04-27 13:08:20 +0200

    Changed plugin to better fit the testing purpose

--- classes/class.ilCustomModalPlugin.php
+++ classes/class.ilCustomModalPlugin.php
@@ -29,22 +29,28 @@ class ilCustomModalPlugin extends ilAppointmentCustomModalPlugin
 	 */
 	public function replaceContent()
 	{
-		//deactivated
-		//return "";
+		//replace content only for courses, so everything else gets extra content added
+        $appointment = $this->getAppointment();
+        $cat_id = ilCalendarCategoryAssignments::_lookupCategory($appointment->getEntryId());
+        $cat = ilCalendarCategory::getInstanceByCategoryId($cat_id);
+
+        if (ilObject::_lookupType($cat->getObjId()) != 'crs') {
+            return "";
+        }
 
 		$appointment = $this->getAppointment();
 
 		if($appointment->isFullDay())
 		{
 			return "<div style='background-color: lightblue; border:3px solid red;padding:10px;'>
- 					<p>[PLUGIN] extra content: This event is FULL DAY!</p>
+ 					<p>Das Plugin 'TestCalendarCustomModalPlugin' ersetzt den Inhalt von ganztägigen Kursen</p>
  					<img src='http://lorempixel.com/300/200/technics' alt=''>
  				</div>";
 		}
 		else
 		{
 			return "<div style='background-color: lightblue; border:1px solid blue;padding:10px;'>
- 					<p>[PLUGIN] extra content: This event is not full day.</p>
+ 					<p>Das Plugin 'TestCalendarCustomModalPlugin' ersetzt den Inhalt von Kursen, die nicht ganztägig sind</p>
  					<img src='http://lorempixel.com/300/200/business' alt=''>
  				</div>";
 		}
@@ -64,17 +70,17 @@ class ilCustomModalPlugin extends ilAppointmentCustomModalPlugin
 
 		if(ilObject::_lookupType($cat->getObjId()) == "sess") {
 			$bgcolor = "orange";
-			$str = "This modal contains Session Information";
+			$str = "Dieses Modal enthält Sitzungsinformationen";
 		}  else if($cat->getType() == ilCalendarCategory::TYPE_OBJ) {
 			$bgcolor = "#DAF0DF";
-			$str = "This modal contains Object info";
+			$str = "Dieses Modal enthält Objektinfo";
 		} else {
 			$bgcolor = "#B5E4EE";
-			$str = "This modal doesn't contain Object info";
+			$str = "Dieses Modal enthält keine Objektinformationen";
 		}
 
 		return "<div style='background-color: $bgcolor; border:1px solid orange;padding:10px;'>
- 					<h1>[PLUGIN] dynamic string: $str</h1>
+ 					<h1>$str</h1>
  					<img src='http://lorempixel.com/400/200/technics' alt=''>
  				</div>";
 	}
@@ -85,7 +91,7 @@ class ilCustomModalPlugin extends ilAppointmentCustomModalPlugin
 	 */
 	public function infoscreenAddContent(ilInfoScreenGUI $a_info)
 	{
-		$a_info->addProperty("[PLUGIN] extra info", "[PLUGIN]This is the value of the property created by the plugin.");
+		$a_info->addProperty("Plugin info", "Dieser Text wird vom Plugin 'TestCalendarCustomModal' erstellt");
 
 		return $a_info;
 	}
@@ -96,7 +102,7 @@ class ilCustomModalPlugin extends ilAppointmentCustomModalPlugin
 	 */
 	public function toolbarAddItems(ilToolbarGUI $a_toolbar)
 	{
-		$a_toolbar->addText("[PLUGIN] txt added");
+		$a_toolbar->addText("Text hinzugefügt vom Plugin 'TestCalendarCustomModal'");
 		return $a_toolbar;
 	}
 
@@ -106,7 +112,7 @@ class ilCustomModalPlugin extends ilAppointmentCustomModalPlugin
 	public function toolbarReplaceContent()
 	{
 		$toolbar = new ilToolbarGUI();
-		$toolbar->addText("[PLUGIN] Toolbar replaced");
+		$toolbar->addText("Toolbar durch das Plugin 'TestCalendarCustomModal' ersetzt");
 		return $toolbar;
 	}
 
@@ -116,6 +122,6 @@ class ilCustomModalPlugin extends ilAppointmentCustomModalPlugin
 	 */
 	public function editModalTitle($title)
 	{
-		return "[PLUGIN]reverses the title: ".strrev($title);
+		return "<span style='color:green'>".mb_strtoupper($title)."</span>";
 	}
 }
